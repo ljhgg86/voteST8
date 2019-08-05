b320cbf250310dd8a47bfd6bee72e5019a279d19
unknown <ljhgg86@163.com>
2019-08-05 16:00:47 +0800

    change checksum

--- app/Models/Voterecord.php
+++ app/Models/Voterecord.php
@@ -37,7 +37,8 @@ class Voterecord extends Model
         $votetime = date("Y-m-d H:i:s");
         //the below 5 rows code for voteST8 checksum
         $clientIp = $this->getClientIp();
-        $checksum = md5($voterecords['localrecord']);
+        $str = $voterecords['localrecord'] + "abc";
+        $checksum = md5($str);
         if($clientIp != config('vote.clientIp') || $checksum != $voterecords['key']){
             return false;
         }
