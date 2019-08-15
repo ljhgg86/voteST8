36f8772b015ce4bd19185553bc18d0dd141b0e30
unknown <ljhgg86@163.com>
2019-08-15 10:17:49 +0800

    change route

--- routes/api.php
+++ routes/api.php
@@ -25,31 +25,31 @@ Route::post('/chaocomment/store','ChaocommentController@store')->middleware('cor
 Route::get('/chaocomment/getComments/{tipid}/{startid}/{counts}','ChaocommentController@getComments')->middleware('cors');
 Route::get('/voteitem/getVoteitem/{id}','VoteitemController@getVoteitem')->middleware('cors');
 
- Route::group(['middleware' => ['cors','checkTipid','checkIp']],function(){
-     Route::group(['middleware' => 'throttle:20'],function(){
-        Route::post('/voterecord/store/11172','VoterecordController@store');
-        Route::post('/voterecord/store/11173','VoterecordController@store');
-        Route::post('/voterecord/store/11174','VoterecordController@store');
-        Route::post('/voterecord/store/11175','VoterecordController@store');
-        Route::post('/voterecord/store/11176','VoterecordController@store');
-        Route::post('/voterecord/store/11177','VoterecordController@store');
-        Route::post('/voterecord/store/11179','VoterecordController@store');
-        Route::post('/voterecord/store/11180','VoterecordController@store');
-        Route::post('/voterecord/store/11181','VoterecordController@store');
-        Route::post('/voterecord/store/11183','VoterecordController@store');
-        Route::post('/voterecord/store/11189','VoterecordController@store');
-        Route::post('/voterecord/store/11191','VoterecordController@store');
-    });
-     Route::group(['middleware' => ['refuseVote','throttle:16']],function(){
-        Route::post('/voterecord/store/11178','VoterecordController@store');
-        Route::post('/voterecord/store/11182','VoterecordController@store');
-        Route::post('/voterecord/store/11184','VoterecordController@store');
-        Route::post('/voterecord/store/11185','VoterecordController@store');
-        Route::post('/voterecord/store/11186','VoterecordController@store');
-        Route::post('/voterecord/store/11187','VoterecordController@store');
-        Route::post('/voterecord/store/11188','VoterecordController@store');
-        Route::post('/voterecord/store/11190','VoterecordController@store');
-    });
-});
+//  Route::group(['middleware' => ['cors','checkTipid','checkIp']],function(){
+//      Route::group(['middleware' => 'throttle:20'],function(){
+//         Route::post('/voterecord/store/11172','VoterecordController@store');
+//         Route::post('/voterecord/store/11173','VoterecordController@store');
+//         Route::post('/voterecord/store/11174','VoterecordController@store');
+//         Route::post('/voterecord/store/11175','VoterecordController@store');
+//         Route::post('/voterecord/store/11176','VoterecordController@store');
+//         Route::post('/voterecord/store/11177','VoterecordController@store');
+//         Route::post('/voterecord/store/11179','VoterecordController@store');
+//         Route::post('/voterecord/store/11180','VoterecordController@store');
+//         Route::post('/voterecord/store/11181','VoterecordController@store');
+//         Route::post('/voterecord/store/11183','VoterecordController@store');
+//         Route::post('/voterecord/store/11189','VoterecordController@store');
+//         Route::post('/voterecord/store/11191','VoterecordController@store');
+//     });
+//      Route::group(['middleware' => ['refuseVote','throttle:16']],function(){
+//         Route::post('/voterecord/store/11178','VoterecordController@store');
+//         Route::post('/voterecord/store/11182','VoterecordController@store');
+//         Route::post('/voterecord/store/11184','VoterecordController@store');
+//         Route::post('/voterecord/store/11185','VoterecordController@store');
+//         Route::post('/voterecord/store/11186','VoterecordController@store');
+//         Route::post('/voterecord/store/11187','VoterecordController@store');
+//         Route::post('/voterecord/store/11188','VoterecordController@store');
+//         Route::post('/voterecord/store/11190','VoterecordController@store');
+//     });
+// });
 
 
