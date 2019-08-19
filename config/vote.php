01cf003271c05016ff22a047cfcbe425375fbdbe
unknown <ljhgg86@163.com>
2019-08-19 10:05:47 +0800

    check code

--- config/vote.php
+++ config/vote.php
@@ -4,16 +4,16 @@ return [
     'logo' =>'橄榄台',
     'weChatType' => 2,
     'readnumIncreMin' => 1,
-    'readnumIncreMax' => 1,
+    'readnumIncreMax' => 2,
     'clientIp' =>'10.16.11.6',
-    'refuseTime' => '2019-08-15 16:30:00',
-    'unIncreVoteArray' => array('11187'),
+    'refuseTime' => '2019-08-15 18:30:00',
+    'unIncreVoteArray' => array(),
     'refuseIpArray' =>array(),
     'refuseMinTime' => '01:00:00',
     'refuseMaxTime' => '06:00:00',
     'tipidsArr' => array('11172','11173','11174','11175','11176','11177','11178','11179','11180','11181','11182','11183','11184','11185','11186','11187','11188','11189','11190','11191'),
     'controllTime' => 900,
     'controllCounts' => 250,
-    'limitCounts' => 50,
-    'limitVoteArr' => array()
+    'limitCounts' => 130,
+    'limitVoteArr' => array('11183','11187')
 ];
